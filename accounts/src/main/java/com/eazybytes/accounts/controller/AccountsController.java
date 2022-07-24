9d1342e2a87e10c1babd80c2f332ede960a78594
Tiến Lê <tien.le@zamo.io>
2022-07-24 18:27:44 +0700

    [Section 7] Make other microservices to load config from config-server

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -3,14 +3,17 @@
  */
 package com.eazybytes.accounts.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.eazybytes.accounts.config.AccountServiceConfig;
 import com.eazybytes.accounts.model.Accounts;
 import com.eazybytes.accounts.model.Customer;
 import com.eazybytes.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Eazy Bytes
@@ -18,10 +21,17 @@ import com.eazybytes.accounts.repository.AccountsRepository;
  */
 
 @RestController
+@AllArgsConstructor
 public class AccountsController {
-	
-	@Autowired
-	private AccountsRepository accountsRepository;
+
+	private final AccountsRepository accountsRepository;
+	private final AccountServiceConfig config;
+
+	@SneakyThrows
+	@GetMapping
+	public String hello() {
+		return config.getMsg();
+	}
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
